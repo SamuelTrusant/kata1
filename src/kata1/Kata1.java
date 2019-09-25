c403cfb659d6c2b56ed4ff01b4ddd025d404fc7f
Usuario <Usuario@10.22.148.88>
2019-09-25 14:30:37 +0100

    project created

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,8 +1,12 @@
 package kata1;
 
+import java.util.Date;
+
 public class Kata1 {
 
     public static void main(String[] args) {
-    }
+        Person samuel = new Person("Samuel", new Date(99, 2, 5));
+        System.out.println(samuel.toString() + " de edad: " + samuel.getAge() + " años");
+    }   
     
 }
