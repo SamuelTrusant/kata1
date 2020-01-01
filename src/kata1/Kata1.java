03900279c0d175f21960cd9a66c66fa577d18ece
samus <samus@192.168.1.135>
2020-01-01 15:23:58 +0000

    versión con LocalDate

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,14 +1,12 @@
 package kata1;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.time.LocalDate;
+import java.time.Period;
 
 public class Kata1 {
 
     public static void main(String[] args) {
-        Calendar date;
-        date = GregorianCalendar.getInstance();
-        date.set(1999, 5, 2);
+        LocalDate date = LocalDate.of(1999, 5, 2);
         
         Person samuel = new Person("Samuel",date);
         System.out.println(samuel.getName() + " tiene " + samuel.getAge() + " años");
--- src/kata1/Person.java
+++ src/kata1/Person.java
@@ -1,14 +1,16 @@
 package kata1;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.time.LocalDate;
+import java.time.Period;
 
 public class Person {
+    private final LocalDate birthday;
+    private Period periodo;
     private final String  name;
-    private final Calendar birthday;
+    //private final Calendar birthday;
     private static final long MS_per_year = (long) (1000 * 60 * 60 * 24 * 365.25);
 
-    public Person(String name, Calendar birthday) {
+    public Person(String name, LocalDate birthday) {
         this.name = name;
         this.birthday = birthday;
     }
@@ -17,13 +19,14 @@ public class Person {
         return name;
     }
 
-    public Calendar getBirthday() {
+    public LocalDate getBirthday() {
         return birthday;
     }
    
     public int getAge(){
-        Calendar today = GregorianCalendar.getInstance();
-        return (int) (msToYears(today.getTimeInMillis() - birthday.getTimeInMillis()));
+        LocalDate today = LocalDate.now();
+        periodo = Period.between(birthday, today);
+        return periodo.getYears();
     }
 
     private long msToYears(long ms) {
